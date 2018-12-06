71f5699f3192e34d64df42d70e880053f77ae8b5
jakob garricks <garricksjakob@gmail.com>
2018-12-05 23:33:26 -0800

    add multiple modes

--- views/Connect4.js
+++ views/Connect4.js
@@ -17,8 +17,9 @@ export default class Connect4 extends React.Component {
 	static navigationOptions = {
     header: null
   }
-    constructor() {
+    constructor({navigation}) {
     	super();
+    	const AI = navigation.getParam('Ai', false); 
         const ds = new ListView.DataSource({
             rowHasChanged: (r1, r2) => r1 !== r2
         })
@@ -38,7 +39,7 @@ export default class Connect4 extends React.Component {
             c5:6,
             c6:6,
             c7:6,
-            Ai:false,
+            Ai: AI ,
         }
         this.renderRow = this.renderRow.bind(this); 
     }
@@ -151,7 +152,6 @@ export default class Connect4 extends React.Component {
             c5:6,
             c6:6,
             c7:6,
-            Ai:false,
 	    })
 		if(home == 2){
 			this.props.navigation.navigate('Home')
